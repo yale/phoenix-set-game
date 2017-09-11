9aa0ff078ba52f02a8207db9b75598ed09d4baff
Yale Spector <yale.spector@gmail.com>
2017-09-11 19:58:03 -0400

    gettin ready for heroku

--- lib/set_game_web/channels/user_socket.ex
+++ lib/set_game_web/channels/user_socket.ex
@@ -5,7 +5,8 @@ defmodule SetGameWeb.UserSocket do
   channel "game:*", SetGameWeb.GameChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket,
+    timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
